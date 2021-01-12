06932d4741d99de08476dc289e9cb90de5d1cd4a
Misk007 <mdrahmanmiskatur@gmail.com>
2021-01-12 13:45:49 +0600

    first commit

---
+++ input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class example {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x, y;
+        System.out.println("x: ");
+        x = input.nextInt();
+        System.out.println("y: ");
+        y = input.nextInt();
+
+        System.out.printf("%d + %d = %d\n", x, y, (x + y));
+    }
+}
