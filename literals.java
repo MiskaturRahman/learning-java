1f58bbf0b8e9d5162d844b3be4175364139f8787
Misk007 <rahmnmisk007@gmail.com>
2021-01-23 23:04:58 +0600

    update

---
+++ literals.java
@@ -0,0 +1,17 @@
+public class literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 67;// doesnot require mention till interger limit
+        long ageDino = 5666666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+        boolean a = true;
+
+        System.out.print(age);// doesnot create new line
+        String str = " Miskatur";
+        System.out.println(str);// println creates new line
+
+    }
+}
